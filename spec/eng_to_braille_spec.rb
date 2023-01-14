642a293621d950532c6a76579e98fdd56445c7f7
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-14 12:46:58 -0700

    Add test for translating the english character to an array of braille characters

--- spec/eng_to_braille_spec.rb
+++ spec/eng_to_braille_spec.rb
@@ -1,12 +1,40 @@
 require 'spec_helper.rb'
 
 RSpec.describe EngToBraille do
+  let(:eng_to_braille) { EngToBraille.new }
   describe "#initialize" do
     it "exists" do
-      eng_to_braille = EngToBraille.new
-
       expect(eng_to_braille).to be_an_instance_of(EngToBraille)
       expect(eng_to_braille.braille_translation).to be_a(Hash)
+    end
+
+    it "can translate english characters to and array of braille characters" do
+      expect(eng_to_braille.braille_translation["a"]).to eq(["0.", "..", ".."])
+      expect(eng_to_braille.braille_translation["b"]).to eq(["0.", "0.", ".."])
+      expect(eng_to_braille.braille_translation["c"]).to eq(["00", "..", ".."])
+      expect(eng_to_braille.braille_translation["d"]).to eq(["00", ".0", ".."])
+      expect(eng_to_braille.braille_translation["e"]).to eq(["0.", ".0", ".."])
+      expect(eng_to_braille.braille_translation["f"]).to eq(["00", "0.", ".."])
+      expect(eng_to_braille.braille_translation["g"]).to eq(["00", "00", ".."])
+      expect(eng_to_braille.braille_translation["h"]).to eq(["0.", "00", ".."])
+      expect(eng_to_braille.braille_translation["i"]).to eq([".0", "0.", ".."])
+      expect(eng_to_braille.braille_translation["j"]).to eq([".0", "00", ".."])
+      expect(eng_to_braille.braille_translation["k"]).to eq(["0.", "..", "0."])
+      expect(eng_to_braille.braille_translation["l"]).to eq(["0.", "0.", "0."])
+      expect(eng_to_braille.braille_translation["m"]).to eq(["00", "..", "0."])
+      expect(eng_to_braille.braille_translation["n"]).to eq(["00", ".0", "0."])
+      expect(eng_to_braille.braille_translation["o"]).to eq(["0.", ".0", "0."])
+      expect(eng_to_braille.braille_translation["p"]).to eq(["00", "0.", "0."])
+      expect(eng_to_braille.braille_translation["q"]).to eq(["00", "00", "0."])
+      expect(eng_to_braille.braille_translation["r"]).to eq(["0.", "00", "0."])
+      expect(eng_to_braille.braille_translation["s"]).to eq([".0", "0.", "0."])
+      expect(eng_to_braille.braille_translation["t"]).to eq([".0", "00", "0."])
+      expect(eng_to_braille.braille_translation["u"]).to eq(["0.", "..", "00"])
+      expect(eng_to_braille.braille_translation["v"]).to eq(["0.", "0.", "00"])
+      expect(eng_to_braille.braille_translation["w"]).to eq([".0", "00", ".0"])
+      expect(eng_to_braille.braille_translation["x"]).to eq(["00", "..", "00"])
+      expect(eng_to_braille.braille_translation["y"]).to eq(["00", ".0", "00"])
+      expect(eng_to_braille.braille_translation["z"]).to eq(["0.", ".0", "00"])
     end
   end
 end
